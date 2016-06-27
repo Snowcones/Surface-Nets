a23f92464ac73e5ec9b2e3e7b93960c2422537fb
William Henning <wchenningwchenning@gmail.com>
2016-06-27 00:07:00 -0400

    Initial Commit

---
+++ Surface Nets/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Surface Nets
+//
+//  Created by William Henning on 6/27/16.
+//  Copyright © 2016 William Henning. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
